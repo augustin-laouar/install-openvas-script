f412162164347dd9bfc14f71ee072f2310e116ce
augustin-laouar <55484260+augustin-laouar@users.noreply.github.com>
2023-10-17 17:19:19 +0200

    Update install_debian.sh

--- install_debian.sh
+++ install_debian.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
+install_dir = $PWD
 source "./prerequisites.sh"
+cd $install_dir
 source "./build_and_install.sh"
+cd $install_dir
 source "./system_setup.sh"
+cd $install_dir
 source "./feed_synchronization.sh"
