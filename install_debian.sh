be1055afe38c38460544e83df1332547b86163a5
augustin-laouar <55484260+augustin-laouar@users.noreply.github.com>
2023-11-07 10:27:16 +0100

    Delete install_debian.sh

--- install_debian.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-install_dir=$PWD
-source "./prerequisites.sh"
-cd $install_dir
-source "./build_and_install.sh"
-cd $install_dir
-source "./system_setup.sh"
-cd $install_dir
-source "./feed_synchronization.sh"
